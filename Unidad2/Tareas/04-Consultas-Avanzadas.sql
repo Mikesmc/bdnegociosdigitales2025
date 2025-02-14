ef7c4f8eee15ff86b0c083772ee52edac6451413
Miguel Angel Santiago Colon <miguelsanti1611@gmail.com>
2025-02-14 16:53:08 -0600

    Logro: Consultas intermedias con inner join terminadas y comienzo de avanzadas

--- Unidad2/Tareas/04-Consultas-Avanzadas.sql
+++ Unidad2/Tareas/04-Consultas-Avanzadas.sql
@@ -197,3 +197,83 @@ inner join [Order Details] as od
 on od.OrderID = o.OrderID
 group by e.FirstName, e.LastName
 
+--11 Listar los clientes y la cantidad de pedidos que han realizado
+
+select c.CustomerID as Cliente, 
+c.ContactName as Nombre, 
+count(o.OrderID) as Total_Pedidos
+from Customers as c
+join Orders as o 
+on c.CustomerID = o.CustomerID
+group by c.CustomerID, c.ContactName
+order by Total_Pedidos desc;
+
+
+--12 Obtener los empleados que han gestionado pedidos enviados a alemania
+
+select distinct concat (e.FirstName, ' ', e.LastName) as [Nombre Empleado]
+from Employees as e
+join Orders as o 
+on e.EmployeeID = o.EmployeeID
+where o.ShipCountry = 'Germany';
+
+
+--13 Listar los productos junto con el nombre del proveedor y el pais de origen
+
+select p.ProductName as Producto, 
+s.CompanyName as Proveedor, 
+s.Country as [Pais Origen]
+from Products as p
+join Suppliers as s 
+on p.SupplierID = s.SupplierID;
+
+--14 Obtener los pedidos agrupados por pais de envio
+
+select  ShipCountry as [Pais de envio],
+count (o.OrderID) as [Total de envio]
+from Orders as o
+group by ShipCountry
+
+
+--15 Obtener los empleados y la cantidad  de territorios en los que trabajan
+
+select e.FirstName as Nombre,
+count(et.TerritoryID) as [Total Territorio]
+from Employees as e
+join EmployeeTerritories as et
+on e.EmployeeID = et.EmployeeID
+group by e.FirstName
+
+select e.FirstName as Nombre,
+t.TerritoryDescription
+,count(et.TerritoryID) as [Total Territorio]
+from Employees as e
+join EmployeeTerritories as et
+on e.EmployeeID = et.EmployeeID
+join Territories as t
+on et.TerritoryID = t.TerritoryID
+group by e.FirstName, t.TerritoryDescription
+order by Nombre, t.TerritoryDescription
+
+--16 Listar las categorias y la cantidad de productos que contienen
+
+select c.CategoryName,
+count(p.ProductID) 
+from Categories as c
+join Products as p
+on p.CategoryID = c.CategoryID
+group by c.CategoryName
+
+--17 Obtener la cantidad total de productos vendidos por proveedor
+
+select s.CompanyName as Proveedor,
+sum(od.Quantity) as [Total vendido] 
+from Suppliers as s
+join Products as p
+on s.SupplierID = p.SupplierID
+join [Order Details] as od
+on od.ProductID = p.ProductID
+group by s.CompanyName
+
+--18 Obtener la cantidad de pedidos enviados por cada empresa de transporte
+
